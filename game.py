159a1440a35cd90991c33a0939b302cc47afa8d2
Dimitar Chaushev <d.chaushev073@gmail.com>
2014-05-31 13:22:27 +0300

    Animated movement and zombie death. Implemented update() methods in Hero and Zombie classes.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame, sys, time, random
+import pygame, sys, time, random, glob
 from pygame.locals import *
 from random import randint
 
@@ -15,15 +15,75 @@ LEFT = 'left'
 SPEED = 2
 ZOMBIESPEED = 1
 
+
+
 class Hero:
 	def __init__(self):
 		self.isAlive = True
-		self.level = 1
-		self.gun = PISTOL
 		self.facing = RIGHT
 		self.x = PLAYERX
 		self.y = PLAYERY
+		self.surface = R_TROOPER_WEAPON
+		self.ani_speed_init = 10
+		self.ani_speed = self.ani_speed_init
+		self.ani = glob.glob("soldier sprite/run/tmp-*.gif")
+		self.ani.sort()
+		self.ani_pos = 0;
+		self.ani_max = len(self.ani) - 1
+		self.update([])
 
+	def update(self, zombies):
+		if self.isAlive:
+			if moveLeft:
+				self.x -= SPEED
+				self.surface = L_TROOPER_WEAPON
+				self.facing = LEFT
+				self.surface = pygame.transform.flip(pygame.image.load(self.ani[self.ani_pos]), True, False)
+			if moveRight:
+				self.x += SPEED
+				self.facing = RIGHT
+				self.surface = pygame.image.load(self.ani[self.ani_pos])
+			if moveUp:
+				self.y -= SPEED
+				if self.facing == LEFT:
+					self.surface = pygame.transform.flip(pygame.image.load(self.ani[self.ani_pos]), True, False)
+				else:
+					self.surface = pygame.image.load(self.ani[self.ani_pos])
+			if moveDown:
+				self.y += SPEED
+				if self.facing == LEFT:
+					self.surface = pygame.transform.flip(pygame.image.load(self.ani[self.ani_pos]), True, False)
+				else:
+					self.surface = pygame.image.load(self.ani[self.ani_pos])
+			if fire:
+				self.fireWeapon(zombies)
+			if self.ani_pos == self.ani_max:
+				self.ani_pos = 0
+			else:
+				self.ani_pos += 1
+
+		else:
+			self.surface = pygame.image.load('soldier sprite/soldier_dead.gif')
+
+		DISPLAYSURF.blit(self.surface, (self.x, self.y))
+
+
+	def fireWeapon(self, zombies):
+		for zombie in zombies:
+			if self.facing == LEFT:
+				self.surface = pygame.image.load('soldier sprite/shoot_left.png')
+				if zombie.x < self.x:
+					if zombie.y <= self.y + 64/2 and zombie.y >= self.y:
+						zombie.isAlive = False
+						zombie.surface = pygame.image.load('zombie sprite/zombie_dead.gif')
+						return
+			if self.facing == RIGHT:
+				self.surface = pygame.transform.flip(pygame.image.load('soldier sprite/shoot_left.png'), True, False)
+				if zombie.x > self.x:
+					if zombie.y <= self.y + 64/2 and zombie.y >= self.y:
+						zombie.isAlive = False
+						zombie.surface = pygame.image.load('zombie sprite/zombie_dead.gif')
+						return
 
 class Zombie:
 	def __init__(self):
@@ -32,12 +92,57 @@ class Zombie:
 		self.facing = RIGHT
 		self.x = random.choice([100, 1000])
 		self.y = randint(10, 750)
+		self.ani_speed_init = 10
+		self.ani_speed = self.ani_speed_init
+		self.ani = glob.glob("zombie sprite/walk/z-walk-left*.png")
+		self.ani_dead = glob.glob("zombie sprite/dead/z-dead-left*.png")
+		self.ani_dead.sort()
+		self.ani.sort()
+		self.ani_pos = 0;
+		self.ani_pos_dead = 0;
+		self.ani_max = len(self.ani) - 1
+		self.ani_max_dead = len(self.ani_dead) - 1
 
+	def update(self, player):
+		
+		if(self.isAlive):
+			if self.x < player.x:
+				self.x += ZOMBIESPEED;
+				self.surface = pygame.transform.flip(pygame.image.load(self.ani[self.ani_pos]), True, False)
+			elif self.x > player.x:
+				self.x -= ZOMBIESPEED
+				self.surface = pygame.image.load(self.ani[self.ani_pos])
+			if self.y < player.y:
+				self.y += ZOMBIESPEED
+			elif self.y > player.y:
+				self.y -= ZOMBIESPEED
+			if self.x == player.x and self.y == player.y:
+				player.isAlive = False
+			if self.ani_pos == self.ani_max:
+				self.ani_pos = 0
+			else:
+		
+				self.ani_pos += 1
+		else:
+			if self.x < player.x:
+				self.surface = pygame.transform.flip(pygame.image.load(self.ani_dead[self.ani_pos_dead]), True, False)
+			elif self.x > player.x:
+				self.surface = pygame.image.load(self.ani_dead[self.ani_pos_dead])
+			if self.ani_pos_dead == self.ani_max_dead:
+				pass
+			else:
+				self.ani_pos_dead += 1
 
+		DISPLAYSURF.blit(self.surface, (self.x, self.y))
 
 def main():
-	global FPSCLOCK, DISPLAYSURF, BASICFONT, TREEIMAGE, R_TROOPER_IDLE, L_TROOPER_IDLE, R_ZOMBIE_WALK, L_ZOMBIE_WALK, zombies, L_TROOPER_WEAPON, R_TROOPER_WEAPON
+	global FPSCLOCK, DISPLAYSURF, BASICFONT, TREEIMAGE, R_TROOPER_IDLE, L_TROOPER_IDLE, R_ZOMBIE_WALK, L_ZOMBIE_WALK, zombies, L_TROOPER_WEAPON, R_TROOPER_WEAPON, moveLeft, moveRight, moveUp, moveDown, fire
 
+	moveLeft = False
+	moveRight = False
+	moveUp = False
+	moveDown = False
+	fire = False
 
 	pygame.init()
 	FPSCLOCK = pygame.time.Clock()
@@ -60,7 +165,7 @@ def main():
 	R_ZOMBIE_WALK = pygame.image.load('zombie sprite/zombie_walk_right.gif')
 	L_ZOMBIE_WALK = pygame.transform.flip(R_ZOMBIE_WALK, True, False)
 
-	L_TROOPER_WEAPON = pygame.image.load('soldier sprite/soldier_shoot.gif')
+	L_TROOPER_WEAPON = pygame.image.load('soldier sprite/stand_left.png')
 	R_TROOPER_WEAPON = pygame.transform.flip(L_TROOPER_WEAPON, True, False)
 
 
@@ -75,15 +180,7 @@ def main():
 
 	for i in zombies:
 		print("({0}, {1})".format(i.x, i.y))
-
-	player.surface = R_TROOPER_WEAPON
 	
-
-	moveLeft = False
-	moveRight = False
-	moveUp = False
-	moveDown = False
-	fire = False
 
 	while True:
 
@@ -108,18 +205,11 @@ def main():
 				elif event.key in (K_LEFT, K_a):
 					moveRight = False
 					moveLeft = True
-					if player.facing == RIGHT:
-						player.surface = L_TROOPER_WEAPON
-						player.facing = LEFT
 				elif event.key in (K_RIGHT, K_d):
 					moveLeft = False
 					moveRight = True
-					if player.facing == LEFT:
-						player.surface = R_TROOPER_WEAPON
-						player.facing = RIGHT
 				elif event.key == K_SPACE:
 					fire = True
-
 			elif event.type == KEYUP:
 				if event.key in (K_UP, K_w):
 					moveUp = False
@@ -132,65 +222,17 @@ def main():
 				elif event.key == K_SPACE:
 					fire = False
 
-		if player.isAlive:
-			if moveLeft:
-				player.x -= SPEED
-			if moveRight:
-				player.x += SPEED
-			if moveUp:
-				player.y -= SPEED
-			if moveDown:
-				player.y += SPEED
-			if fire:
-				fireWeapon(player, zombies)
-		else:
-			player.surface = pygame.image.load('soldier sprite/soldier_dead.gif')
 
 
-		zombieMove(zombies, player)
+		player.update(zombies)
+
 
 		
 		for zombie in zombies:
-			DISPLAYSURF.blit(zombie.surface, (zombie.x, zombie.y))
-		DISPLAYSURF.blit(player.surface, (player.x, player.y))
-
+			zombie.update(player)
 
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
-
-def zombieMove(zombies, player):
-	for zombie in zombies:
-		if(zombie.isAlive):
-			if zombie.x < player.x:
-				zombie.x += ZOMBIESPEED;
-				zombie.surface = R_ZOMBIE_WALK
-			elif zombie.x > player.x:
-				zombie.x -= ZOMBIESPEED
-				zombie.surface = L_ZOMBIE_WALK
-			if zombie.y < player.y:
-				zombie.y += ZOMBIESPEED
-			elif zombie.y > player.y:
-				zombie.y -= ZOMBIESPEED
-			if zombie.x == player.x and zombie.y == player.y:
-				player.isAlive = False
-
-def fireWeapon(player, zombies):
-	for zombie in zombies:
-		if player.facing == LEFT:
-			player.surface = pygame.image.load('soldier sprite/soldier_shoot_left.gif')
-			if zombie.x < player.x:
-				if zombie.y <= player.y + 64/2 and zombie.y >= player.y:
-					zombie.isAlive = False
-					zombie.surface = pygame.image.load('zombie sprite/zombie_dead.gif')
-					return
-		if player.facing == RIGHT:
-			player.surface = pygame.transform.flip(pygame.image.load('soldier sprite/soldier_shoot_left.gif'), True, False)
-			if zombie.x > player.x:
-				if zombie.y <= player.y + 64/2 and zombie.y >= player.y:
-					zombie.isAlive = False
-					zombie.surface = pygame.image.load('zombie sprite/zombie_dead.gif')
-					return
-
 
 
 if __name__ == '__main__':
