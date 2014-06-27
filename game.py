dc58818ffc69510e601313044057c18e4e04a530
Dimitar Chaushev <d.chaushev073@gmail.com>
2014-06-27 19:59:38 +0300

    changed game.py file - game starts with menu

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ import enumerations
 from enumerations import *
 import Zombie
 from Zombie import *
+from menu import *
 
 def fire(player, zombies):
     if player.facing == RIGHT:
@@ -45,83 +46,7 @@ def gameOver():
     DISPLAYSURF.blit(text, [text_x, text_y])
 
 
-def gameplay(player, zombies):
-
-    DISPLAYSURF.fill(TERRAINCOLOR)
-    DISPLAYSURF.blit(player.surface, (player.x, player.y))
-
-    gamefont = pygame.font.Font(None, 30)
-    scoretext = gamefont.render('Score: ' + str(player.score), 2, [0, 0, 0])
-    boxsize = scoretext.get_rect()
-    scoreXpos = 900
-
-    DISPLAYSURF.blit(scoretext, (scoreXpos, 20))
-
-    if player.is_alive:
-
-        if not zombies:
-            zombie_count += 5
-            for i in range(zombie_count):
-                zombie = Zombie()
-                zombies.append(zombie)
-
-        zombies = [z for z in zombies if z.animation_dead_position < z.animation_dead_max]
-        for z in zombies:
-            DISPLAYSURF.blit(z.surface, (z.x, z.y))
-            box = pygame.Rect(z.x, z.y, (z.health*z.surface.get_width())/10, -5)
-            pygame.draw.rect(DISPLAYSURF, (255, 0, 0), box, 0)
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == KEYDOWN:
-            if event.key in (K_UP, K_w):
-                player.move_down = False
-                player.move_up = True
-            elif event.key in (K_DOWN, K_s):
-                player.move_up = False
-                player.move_down = True
-            elif event.key in (K_LEFT, K_a):
-                player.move_right = False
-                player.move_left = True
-            elif event.key in (K_RIGHT, K_d):
-                player.move_left = False
-                player.move_right = True
-            elif event.key == K_SPACE:
-                player.fire = True
-                fire(player, zombies)
-        elif event.type == KEYUP:
-            if event.key in (K_UP, K_w):
-                player.move_up = False
-            elif event.key in (K_DOWN, K_s):
-                player.move_down = False
-            elif event.key in (K_RIGHT, K_d):
-                player.move_right = False
-            elif event.key in (K_LEFT, K_a):
-                player.move_left = False
-            elif event.key == K_SPACE:
-                player.fire = False
-
-    player.update()
-
-    for z in zombies:
-        dead = z.update(player.x, player.y)
-        if dead:
-            player.is_alive = False
-
-    pygame.display.update()
-    FPSCLOCK.tick(FPS)
-
-
-def main():
-
-    global FPSCLOCK, DISPLAYSURF, zombie_count
-
-    pygame.init()
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption('The Last Stand')
+def gameplay():
 
     zombie_count = 10
     player = Hero()
@@ -133,7 +58,75 @@ def main():
     scoreIncrementTimer = 0
     lastFrameTicks = pygame.time.get_ticks()
 
-    while True:
+    while 1:
+
+        DISPLAYSURF.fill(TERRAINCOLOR)
+        DISPLAYSURF.blit(player.surface, (player.x, player.y))
+
+        gamefont = pygame.font.Font(None, 30)
+        scoretext = gamefont.render('Score: ' + str(player.score), 2, [0, 0, 0])
+        boxsize = scoretext.get_rect()
+        scoreXpos = 900
+
+        DISPLAYSURF.blit(scoretext, (scoreXpos, 20))
+
+        if player.is_alive:
+
+            if not zombies:
+                zombie_count += 5
+                for i in range(zombie_count):
+                    zombie = Zombie()
+                    zombies.append(zombie)
+
+            zombies = [z for z in zombies if z.animation_dead_position < z.animation_dead_max]
+            for z in zombies:
+                DISPLAYSURF.blit(z.surface, (z.x, z.y))
+                box = pygame.Rect(z.x, z.y, (z.health*z.surface.get_width())/10, -5)
+                pygame.draw.rect(DISPLAYSURF, (255, 0, 0), box, 0)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key in (K_UP, K_w):
+                    player.move_down = False
+                    player.move_up = True
+                elif event.key in (K_DOWN, K_s):
+                    player.move_up = False
+                    player.move_down = True
+                elif event.key in (K_LEFT, K_a):
+                    player.move_right = False
+                    player.move_left = True
+                elif event.key in (K_RIGHT, K_d):
+                    player.move_left = False
+                    player.move_right = True
+                elif event.key == K_SPACE:
+                    player.fire = True
+                    fire(player, zombies)
+                elif event.key == K_ESCAPE:
+                    return
+            elif event.type == KEYUP:
+                if event.key in (K_UP, K_w):
+                    player.move_up = False
+                elif event.key in (K_DOWN, K_s):
+                    player.move_down = False
+                elif event.key in (K_RIGHT, K_d):
+                    player.move_right = False
+                elif event.key in (K_LEFT, K_a):
+                    player.move_left = False
+                elif event.key == K_SPACE:
+                    player.fire = False
+
+        player.update()
+
+        for z in zombies:
+            dead = z.update(player.x, player.y)
+            if dead:
+                player.is_alive = False
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
 
         thisFrameTicks = pygame.time.get_ticks()
         ticksSinceLastFrame = thisFrameTicks - lastFrameTicks
@@ -144,12 +137,55 @@ def main():
             player.score += 1
             scoreIncrementTimer = 0
 
-        gameplay(player, zombies)
+
+
+def main():
+
+    global FPSCLOCK, DISPLAYSURF, zombie_count
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('The Last Stand')
+
+
+    menu = RotatingMenu(x=320, y=240, radius=220, arc=pi, defaultAngle=pi/2.0)
+    menu.addItem(MenuItem("New Game"))
+    menu.addItem(MenuItem("High Scores"))
+    menu.addItem(MenuItem("Exit"))
+    menu.selectItem(0)
+
+    while True:
+        show_menu(menu)
 
 
 
+def show_menu(menu):
 
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                menu.selectItem(menu.selectedItemNumber + 1)
+            if event.key == pygame.K_RIGHT:
+                menu.selectItem(menu.selectedItemNumber - 1)
+            if event.key == pygame.K_KP_ENTER:
+                print(menu.selectedItemNumber)
+                if menu.selectedItemNumber == 2:
+                    pygame.quit()
+                    sys.exit()
+                elif menu.selectedItemNumber == 0:
+                    gameplay()
 
+    menu.update()
+
+    #Draw stuff
+    DISPLAYSURF.fill((0,0,0))
+    menu.draw(DISPLAYSURF)
+    pygame.display.flip()
 
 if __name__ == '__main__':
     main()
